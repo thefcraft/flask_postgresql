9365894842751de648a5cadc45a2852031381a37
Thefcraft <76678136+thefcraft@users.noreply.github.com>
2024-03-16 02:34:08 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="flask-pgsql",
-    version="0.0.1",
+    version="0.0.2",
     author="ThefCraft",
     author_email="sisodiyalaksh@gmail.com",
     url="https://github.com/thefcraft/flask_postgresql",
